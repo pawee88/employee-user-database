2ca1c01e52ef186ef88afa2965f2d7d1e683af46
pawee88 <68136483+pawee88@users.noreply.github.com>
2021-08-11 23:03:50 -0600

    First Commit

---
+++ server/controller/controller.js
@@ -0,0 +1,130 @@
+var Userdb = require('../model/model');
+
+// Create and save new user
+
+exports.create = (req,res)=>{
+    //VAlidate request
+
+    if(!req.body){
+        res.status(400).send({message: "Content can not be empty!"});
+        return;
+    }
+
+    //new user
+
+    const user = new Userdb({
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        position:req.body.position,
+        gender:req.body.gender,
+        status:req.body.status
+    })
+
+    //save user in the database
+
+    user
+        .save(user)
+        .then(data=>{
+            // res.send(data)
+            res.redirect('/add-user');
+        })
+        .catch(err=>{
+            res.status(500).send({
+                message:err.message || "Some error occurred while creating a create operation"
+            });
+        });
+
+
+
+
+
+
+}
+
+// Retrieve and return all users
+
+exports.find = (req, res)=>{
+
+
+if(req.query.id){
+    const id = req.query.id;
+
+    Userdb.findById(id)
+    .then(data=>{
+        if(!data){
+            res.status(404).send({message: "Not found user with id" + id})
+        }else{
+            res.send(data)
+        }
+       
+    })
+
+    .catch(err=>{
+        res.status(500).send({message: "Error retrieving user id id" + id})
+    })
+
+}else{
+    Userdb.find()
+    .then(user=>{
+        res.send(user)
+    })
+    .catch(err=>{
+        res.status(500).send({message: err.message || "Error occured while retrieving user information"})
+    })
+
+}
+
+    
+}
+
+//Update a new identified user by user id
+
+
+exports.update = (req,res)=>{
+    if(!req.body){
+        return res
+        .status(400)
+        .send({message: "Data cannot be empty"});
+
+        
+
+    }
+
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id,req.body,{useFindAndModify:false})
+    .then(data =>{
+        if(data){
+            res.status(404).send({message:`Cannot update user with ${id}.Maybe user not found`})
+        }else{
+            res.send(data)
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({message: "Error Update user information"})
+    })
+}
+
+//Delete a user with specified user id in the request
+
+exports.delete = (req,res)=>{
+    const id = req.params.id;
+
+        Userdb.findByIdAndDelete(id)
+        .then(data =>{
+            if(!data){
+                res.status(404).send({message: `Cannot Delete with Id ${id}, Maybe id is wrong`})
+            }else{
+                res.send({
+                    message:"User was deleted successfully"
+                })
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({
+                message:"Could not delete User with id=" + id
+            });
+        });
+   
+
+}
---
+++ server/model/model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    email: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type:Number,
+        required: true,
+        unique: true
+    },
+    position: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    gender:String,
+    status:String
+})
+
+const Userdb = mongoose.model('userdb',schema);
+
+module.exports = Userdb;
